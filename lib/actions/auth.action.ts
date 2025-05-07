39d9d20483546528398d80d2fcd7d272f5767863
Nikhil Kumar Siwakoti <nikhilkumarsiwakoti@gmail.com>
2025-05-07 13:22:50 +0545

    Api For fetching interivews

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -132,3 +132,14 @@ export async  function isAuthenticated(){
 
     return !!user; //ture --> boolean
 }
+
+
+export async function getInterviewByUserId( userId : string ) : Promise<Interview[] | null>{
+    const interviews = await db.collection('interviews').where('userId', '==',  userId).orderBy('createdAt', 'desc').get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+
+    })) as Interview[];
+}
