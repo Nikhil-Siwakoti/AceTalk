0a060ff75c9bcfd462ec6fcc240abbbd54e8599d
Nikhil Kumar Siwakoti <nikhilkumarsiwakoti@gmail.com>
2025-05-07 13:37:54 +0545

    Fetched interview in ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,14 @@ import React from "react";
 import Image from "next/image";
 import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import { getCurrentUser, getInterviewByUserId } from "@/lib/actions/auth.action";
 
-const page = () => {
+const page = async () => {
+    const user = await getCurrentUser();
+    const userInterviews = await getInterviewByUserId(user?.id!);
+
+    const hasPastInterviews = userInterviews?.length > 0 ; 
+
   return (
     <>
       <section className="card-cta">
@@ -29,10 +35,13 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-         {dummyInterviews.map((interview)=>(
-          <InterviewCard key={interview.id}{...interview}/>
-         ))}
-         {/* { <p>You haven&apos;t taken any interview yet.</p>} */}
+         {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard key={interview.id}{...interview}/>
+            ))) : ( <p>You haven&apos;t taken any interview yet.</p>)
+
+          }
+       
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
